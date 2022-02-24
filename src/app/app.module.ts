f542800539dd77d23b3732bc57b9f451799db5ff
Narciso Gomes <narcisoalmeida25@gmail.com>
2022-02-24 01:21:27 -0300

    configuracao ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
+import { appReducers } from './store/index';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from "@angular/material/button";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PersonComponent } from './person/person.component';
+import { StoreModule } from '@ngrx/store';
 
 
 
@@ -21,7 +24,11 @@ import { PersonComponent } from './person/person.component';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    StoreModule.forRoot( appReducers ),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,5 +1,12 @@
+import { ActionReducerMap } from '@ngrx/store';
 import { Person } from '../interfaces/person';
+import * as fromPersonReducer from './person.reducer';
+
 export interface AppState{
   people: Person[]
 
+}
+
+export const appReducers : ActionReducerMap<AppState, any> = {
+  people: fromPersonReducer.reducer
 }
