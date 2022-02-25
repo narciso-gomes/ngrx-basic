5e442f27e84de63de468bb2277cfa405d02a6ccf
Narciso Gomes <narcisoalmeida25@gmail.com>
2022-02-24 22:26:23 -0300

    dispatch new person

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
+import { PersonNew } from './store/person.actions';
+import { AppState } from './store/index';
 import { Person } from './interfaces/person';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+// @ts-ignore
+import * as faker from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +16,19 @@ export class AppComponent {
 
   people$: Observable<Person[]> = new Observable();
 
-  addNew() {
+  constructor(private store: Store<AppState>){}
 
+  addNew() {
+    let person: Person = {
+      name: faker.name.findName(),
+      address: faker.address.streetAddress(),
+      city: faker.address.city(),
+      country: faker.address.country(),
+      age: Math.round(Math.random() * 100),
+      _id: new Date().getMilliseconds().toString()
+    }
+
+    this.store.dispatch(new PersonNew({person}))
   }
 
   update(p: Person) {
